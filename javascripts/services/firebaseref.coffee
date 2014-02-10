51fe01b5dbeed365d4e58c0eaadf65f24892528d
Philipp Bosch <hello@pb.io>
2014-02-10 12:41:50 +0100

    Remove connection check. Did not work reliably.

--- javascripts/services/firebaseref.coffee
+++ javascripts/services/firebaseref.coffee
@@ -4,15 +4,5 @@ angular
     .module('synpad.services.firebaseref', [])
 
     .factory('firebaseref', ['$firebase', '$timeout', '$state', 'dbname', ($firebase, $timeout, $state, dbname) ->
-        fb = new Firebase("https://#{dbname}-synpad.firebaseio.com/")
-
-        connCheck = $timeout ->
-            console.log('No connection!')
-            $state.go('setup')
-        , 2000
-
-        $firebase(fb).$on 'loaded', ->
-            $timeout.cancel(connCheck)
-
-        fb
+        new Firebase("https://#{dbname}-synpad.firebaseio.com/")
     ])
--- public/javascripts/services/firebaseref.js
+++ public/javascripts/services/firebaseref.js
@@ -2,16 +2,7 @@
   "use strict";
   angular.module('synpad.services.firebaseref', []).factory('firebaseref', [
     '$firebase', '$timeout', '$state', 'dbname', function($firebase, $timeout, $state, dbname) {
-      var connCheck, fb;
-      fb = new Firebase("https://" + dbname + "-synpad.firebaseio.com/");
-      connCheck = $timeout(function() {
-        console.log('No connection!');
-        return $state.go('setup');
-      }, 2000);
-      $firebase(fb).$on('loaded', function() {
-        return $timeout.cancel(connCheck);
-      });
-      return fb;
+      return new Firebase("https://" + dbname + "-synpad.firebaseio.com/");
     }
   ]);
 
